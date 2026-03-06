fbb8c4e7d13c96733e77a2c03087e4ce8585d711
choiyujeong <choiyujeong0411@gmail.com>
2026-03-06 15:59:05 +0900

    duckdb 설정 중

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+# 프로젝트 루트 경로
+PROJECT_ROOT = r'C:\Users\choiy\Desktop\aegis_prac'
+os.chdir(PROJECT_ROOT)
+
+DB_PATH = 'data/sepsis.duckdb'
+DATA_PATH = 'data/training'
+OUTPUT_PATH = 'output'
